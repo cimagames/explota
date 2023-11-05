dbcc8e089e22f28c93e54ba4e446a107075e3e7e
santyafull <140284677+santyafull@users.noreply.github.com>
2023-11-05 10:48:27 -0300

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1699147818,
+	"version": 1699192009,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -7,11 +7,6 @@
 		"offlineClient.js",
 		"images/particles.png",
 		"images/particles2.png",
-		"icon-16.png",
-		"icon-32.png",
-		"icon-114.png",
-		"icon-128.png",
-		"icon-256.png",
-		"loading-logo.png"
+		"cvb.png"
 	]
 }
